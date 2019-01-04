9b62583b051db0289de60fa811543ccbe8e268de
bikramkoju <kojubikram@gmail.com>
2019-01-04 15:08:03 +0545

    firstCommit

---
+++ app/src/main/java/com/bikramkoju/interfacelife/OnConfirmDeleteListener.java
@@ -0,0 +1,10 @@
+package com.bikramkoju.interfacelife;
+
+/**
+ * -> Created by Bikram Koju on 24-04-17.
+ */
+
+public interface OnConfirmDeleteListener {
+    void onDelete();
+    void onCancel();
+}
---
+++ app/src/main/java/com/bikramkoju/interfacelife/OnSelectListener.java
@@ -0,0 +1,11 @@
+package com.bikramkoju.interfacelife;
+
+/**
+ * -> Created by Bikram Koju on 23-04-17.
+ */
+
+public interface OnSelectListener {
+    void onEdit();
+    void onDelete();
+    void onCancel();
+}
